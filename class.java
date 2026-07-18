b9ae8e1f0f6e84e767259dbc778326387c6b4fb7
hariAG420 <hhari200297@gmail.com>
2026-07-18 11:30:18 +0530

    Add files via upload

---
+++ class.java
@@ -0,0 +1,18 @@
+public class swap numbers
+{
+public static void main(string[]args)
+{
+int a=10;
+int b=20;
+int temp;
+system.out.println("before swapping:");
+system.out.println("a="+a);
+system.out.println("b="+b);
+temp=a;
+a=b;
+b=temp;
+system.out.println("\n after swapping:");
+system.out.println("a="+a);
+system.out.println("b="+b);
+}
+}
